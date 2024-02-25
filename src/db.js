3e98edb5f9c1abe5e624e15843e23080b645fe34
Leighton8807 <joge.8807@gmail.com>
2024-02-25 02:41:19 -0500

    connection mongoDB atlas already

--- src/db.js
+++ src/db.js
@@ -1,10 +1,30 @@
-import mongoose from "mongoose";
+import mongoose from'mongoose';
+
+const mongoURI = 'mongodb+srv://gruapp_database:NofbfBbgoUdFqAuC@gruappdata.x2nvprw.mongodb.net/?retryWrites=true&w=majority&appName=gruappdata';
 
 export const connectDB = async () => {
-    try{
-        await mongoose.connect('mongodb://localhost/fullstackdb');
-        console.log(">>> DB is connected");
-    }catch (error){
-        console.log(error);
+    try {
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            ssl: true,
+            
+        });
+        console.log('Conexión a MongoDB Atlas establecida');
+    } catch (error) {
+        console.error('Error al conectar a MongoDB Atlas:', error.message);
+        process.exit(1);
     }
-}
+};
+ 
+//NofbfBbgoUdFqAuC
+
+
+// export const connectDB = async () => {
+//     try{
+//         await mongoose.connect('mongodb://localhost/fullstackdb');
+//         console.log(">>> DB is connected");
+//     }catch (error){
+//         console.log(error);
+//     }
+// }
